5ee8206cf5f0c953eb35b2691af0632873892d66
Emerson Albornoz <emersonalbornozalvarez@gmail.com>
2025-09-24 11:18:16 -0500

    Revert "agrego las clases para el modulo de ahorros"
    This reverts commit c56420f5d0d7acbbfb72d186fa183878b6878f86.

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://rmuxgjqxizvzktniedmk.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtdXhnanF4aXp2emt0bmllZG1rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc3OTk1MDgsImV4cCI6MjA3MzM3NTUwOH0.AKWhOQ5QPM5gcu-u0BDRLW-_PHVNhrZRugreOuPtaAI'
+const supabaseUrl = 'llll'
+const supabaseKey = 'llll'
 export const supabase = createClient(supabaseUrl, supabaseKey)
